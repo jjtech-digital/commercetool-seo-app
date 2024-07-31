aed477503d1a70f346664e4f738467ef7f2f4dd3
JiyaAgrawal20 <jiya@compose.co.in>
2024-08-01 01:52:36 +0530

    chore: empty commit

--- src/api/fetchersFunction/utils.ts
+++ src/api/fetchersFunction/utils.ts
@@ -1,18 +1,30 @@
-import { LS_KEY, descriptionPattern, featuresPattern, normalDescPattern, titlePattern } from "../../constants";
+import {
+  LS_KEY,
+  descriptionPattern,
+  featuresPattern,
+  normalDescPattern,
+  titlePattern,
+} from '../../constants';
 
 export const openAiKey = localStorage.getItem(LS_KEY.OPEN_AI_KEY);
-export const setNotification = (setState: Function, message: string, type: string) => {
-    setState((prev: any) => ({
-      ...prev,
-      notificationMessage: message,
-      notificationMessageType: type,
-    }));
-  }
+export const setNotification = (
+  setState: Function,
+  message: string,
+  type: string
+) => {
+  setState((prev: any) => ({
+    ...prev,
+    notificationMessage: message,
+    notificationMessageType: type,
+  }));
+};
 
 export const batchSize = 20;
 
-
-type GenerateMetaDataFunction = (id: string, dataLocale: string) => Promise<any>;
+type GenerateMetaDataFunction = (
+  id: string,
+  dataLocale: string
+) => Promise<any>;
 
 export const processBatches = async (
   productIds: string[],
@@ -22,7 +34,7 @@ export const processBatches = async (
   setState: Function,
   successHandler: (data: any[]) => void,
   errorMessage: string
- )  => {
+) => {
   const totalBatches = Math.ceil(productIds?.length / batchSize);
 
   for (let i = 0; i < totalBatches; i++) {
@@ -36,9 +48,9 @@ export const processBatches = async (
       });
 
       const data = await Promise.all(response);
-      console.log("data",data)
+      console.log('data', data);
       const has401Error = data?.some((res: any) => res?.data?.status === 401);
-      console.log("has401Error",has401Error)
+      console.log('has401Error', has401Error);
       if (has401Error) {
         setState((prev: any) => ({
           ...prev,
@@ -48,9 +60,8 @@ export const processBatches = async (
         return null;
       }
 
-
       successHandler(data);
-      return data
+      return data;
     } catch (error) {
       setState((prev: any) => ({
         ...prev,
@@ -60,32 +71,31 @@ export const processBatches = async (
       console.error(errorMessage, error);
     }
   }
-}
-
+};
 
 export const matchData = (response) => {
-    const metaData = response?.choices?.[0]?.message?.content;
-  
-    const featuresMatch = metaData?.match(featuresPattern);
-    const keyFeatures = featuresMatch ? featuresMatch[1].trim() : null;
-  
-    const descriptionMatch = metaData?.match(normalDescPattern);
-    const description = descriptionMatch ? descriptionMatch[1].trim() : null;
-    return {
-      keyFeatures,
-      description,
-    };
-  };
+  const metaData = response?.choices?.[0]?.message?.content;
 
-  export const seoMatchData = (response) => {
-    const message = response?.choices?.[0]?.message?.content;
-    const titleMatch = message?.match(titlePattern);
-    const title = titleMatch ? titleMatch[2]?.trim() : null;
+  const featuresMatch = metaData?.match(featuresPattern);
+  const keyFeatures = featuresMatch ? featuresMatch[1].trim() : null;
 
-    const descriptionMatch = message?.match(descriptionPattern);
-    const description = descriptionMatch ? descriptionMatch[2]?.trim() : null;
-    return {
-      title,
-      description,
-    };
+  const descriptionMatch = metaData?.match(normalDescPattern);
+  const description = descriptionMatch ? descriptionMatch[1].trim() : null;
+  return {
+    keyFeatures,
+    description,
   };
+};
+
+export const seoMatchData = (response) => {
+  const message = response?.choices?.[0]?.message?.content;
+  const titleMatch = message?.match(titlePattern);
+  const title = titleMatch ? titleMatch[2]?.trim() : null;
+
+  const descriptionMatch = message?.match(descriptionPattern);
+  const description = descriptionMatch ? descriptionMatch[2]?.trim() : null;
+  return {
+    title,
+    description,
+  };
+};
