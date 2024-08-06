60e2ccce398d183429c3099ced128aae5b236678
JiyaAgrawal20 <jiya@compose.co.in>
2024-08-06 14:42:20 +0530

    testing

--- src/components/SettingsData/SettingsOpenAiData.tsx
+++ src/components/SettingsData/SettingsOpenAiData.tsx
@@ -14,10 +14,10 @@ import {
 import { useApplicationContext } from '@commercetools-frontend/application-shell-connectors';
 const SettingsOpenAiData = () => {
   const CTP_API_URL = useApplicationContext(
-    (context) => context.environment.CTP_API_URL
+    (context) => context.environment.apiUrl
   );
   const CTP_PROJECT_KEY = useApplicationContext(
-    (context) => context.environment.CTP_PROJECT_KEY
+    (context) => context.environment.projectKey
   );
 
   const secrets = {
