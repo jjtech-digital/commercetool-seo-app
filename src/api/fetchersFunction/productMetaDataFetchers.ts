bc5ad03e33df7fc338484a259a42d61e60ebfc4f
JiyaAgrawal20 <155526414+JiyaAgrawal20@users.noreply.github.com>
2024-08-27 18:39:18 +0530

    Fix key features array (#58)
    * fix: fixed array for key features
    
    * fix: fix ag grid data

--- src/api/fetchersFunction/productMetaDataFetchers.ts
+++ src/api/fetchersFunction/productMetaDataFetchers.ts
@@ -19,6 +19,15 @@ export const updateProductMeta = async (
   secrets: any,
   setState?: Function
 ) => {
+  function mdFeatures(keyFeatures : string) {
+    const matches = keyFeatures?.matchAll(/-\s\*\*([^*]+)\*\*:\s(.+?)(?=\n-\s\*\*|$)/gs);
+    const result = [];
+    for (const match of matches) {
+      result.push(`${match[1]}: ${match[2]}`);
+    }
+    return result;
+  }
+  const featuresArray = mdFeatures(keyFeatures);
   const { CTP_API_URL, CTP_PROJECT_KEY } = secrets;
   const accessToken = localStorage.getItem(LS_KEY.CT_OBJ_TOKEN);
 
@@ -40,8 +49,13 @@ export const updateProductMeta = async (
     productResponse?.masterData?.current?.masterVariant.attributesRaw.find(
       (item: any) => item.name === 'features'
     );
-  if (existingFeatures?.value?.[0]) {
-    existingFeatures.value[0][dataLocale] = keyFeatures || ' ';
+  if (existingFeatures?.value) {
+    existingFeatures = { name: 'features', value: [{ [dataLocale]: '' }] };
+    const features = { name: 'features', value: [{ [dataLocale]: '' }] };
+    productResponse?.masterData?.current?.masterVariant.attributesRaw.push(
+      features
+    );
+    existingFeatures.value[0][dataLocale] = featuresArray || [];
   }
   if (!existingFeatures) {
     existingFeatures = { name: 'features', value: [{ [dataLocale]: '' }] };
@@ -49,18 +63,17 @@ export const updateProductMeta = async (
     productResponse?.masterData?.current?.masterVariant.attributesRaw.push(
       features
     );
-    existingFeatures.value[0][dataLocale] = keyFeatures || ' ';
+    existingFeatures.value[0][dataLocale] = featuresArray || [];
   }
 
   keyFeaturesObj = existingFeatures.value[0];
+  const features = [keyFeaturesObj];
+  const transformedValueForFeatures = features[0][dataLocale].map((item : any) => ({ [dataLocale]: item }));
   const apiUrl = `${CTP_API_URL}/${CTP_PROJECT_KEY}/products/${productId}`;
   const headers = {
     Authorization: `Bearer ${accessToken}`,
     'Content-Type': 'application/json',
   };
-
-  const x = [keyFeaturesObj];
-
   const payload = {
     version: version,
     actions: [
@@ -72,7 +85,7 @@ export const updateProductMeta = async (
       {
         action: 'setAttributeInAllVariants',
         name: 'features',
-        value: x,
+        value: transformedValueForFeatures,
         staged: false,
       },
     ],
@@ -142,7 +155,7 @@ export const queryProductOpenAi = async (
   }
 
   let fallbackDescriptionPrompt = "Give a user engaging description which would be able to put forward the proper explaination about the product and spark curiosity in incoming traffic. Limit the description to under 150-160 characters for full visibility in search results. Include the main keyword in description according to the its attributes. You must Highlight Key Features, Showcase Benefits, Use Clear, Direct Language, Sensory Description, Call to Action: Every product description should end with a strong call to action encouraging the reader to make a purchase or learn more."
-  let fallbackKeyFeaturesPrompt = "Give 5 or more key features, they should be separated with a comma. All the features should Be Specific, Showcase Functionality, Highlight Quality, Sustainability, Spotlight Innovation according to the given attributes."
+  let fallbackKeyFeaturesPrompt = "Give 5 or more key features, they should be in markdown format. All the features should Be Specific, Showcase Functionality, Highlight Quality, Sustainability, Spotlight Innovation according to the given attributes."
 
   let contentString = `Generate some key features and a precise description for a product with ${query}. The format for the output should be like this - *Description*:abc and *Key Features*:abc". The rules for description are : ${updatedPromptDescription || fallbackDescriptionPrompt}. And the rules for key features are: ${updatedPromptKeyfeatures || fallbackKeyFeaturesPrompt}`;
 
